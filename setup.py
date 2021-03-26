6205a33368e1e5b73a20e9708b4fc46df84adc92
Tonye Jack <jtonye@ymail.com>
2021-03-26 15:55:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   version='0.0.1',
   description='A Python package for getting list of numbers from 1 to 100',
   url='https://github.com/tj-python/github-yaml-deploy',
-  entry_points={'console_scripts': ['counter=deploy:main']},
+  entry_points={'console_scripts': ['github-deploy=deploy:main']},
   keywords=['counter', 'programming language ranking', 'index', 'programming language'],
   author='Tonye Jack',
   author_email='jtonye@ymail.com',
