b28fa60c5323f38696697aff40c7078cfe0fea1d
Tonye Jack <jtonye@ymail.com>
2021-03-26 15:53:20 -0400

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -4,10 +4,10 @@ import click
 @click.option('--count', default=1, help='Number of greetings.')
 @click.option('--name', prompt='Your name',
               help='The person to greet.')
-def hello(count, name):
+def main(count, name):
     """Simple program that greets NAME for a total of COUNT times."""
     for x in range(count):
         click.echo('Hello %s!' % name)
 
 if __name__ == '__main__':
-    hello()
+    main()
