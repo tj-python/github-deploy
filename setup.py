8b1a04a47e6e7630c875286a5be26e0bb33688bf
Tonye Jack <jtonye@ymail.com>
2021-03-26 15:52:54 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+
+setup(
+  name='counter',
+  version='0.0.1',
+  description='A Python package for getting list of numbers from 1 to 100',
+  url='https://github.com/tj-python/github-yaml-deploy',
+  entry_points={'console_scripts': ['counter=deploy:main']},
+  keywords=['counter', 'programming language ranking', 'index', 'programming language'],
+  author='Tonye Jack',
+  author_email='jtonye@ymail.com',
+  license='MIT',
+  packages=['counter'],
+  install_requires=['requests'],
+)
