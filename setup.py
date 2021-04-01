da918ae5f3b671c91b34319763331c6f88c27dd7
Tonye Jack <jtonye@ymail.com>
2021-04-01 09:29:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
+import os
 from setuptools import setup, find_packages
+
+BASE_DIR = os.path.dirname(__file__)
+README_PATH = os.path.join(BASE_DIR, "README.md")
+LONG_DESCRIPTION_TYPE = "text/markdown"
+
+if os.path.isfile(README_PATH):
+    with io.open(README_PATH, encoding="utf-8") as f:
+        LONG_DESCRIPTION = f.read()
+else:
+    LONG_DESCRIPTION = ""
 
 
 setup(
     name="github-deploy",
     version="0.0.1",
     description="Deploy yaml files to a large number of repositories in seconds.",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESCRIPTION_TYPE,
     url="https://github.com/tj-python/github-deploy",
     entry_points={
         "console_scripts": [
