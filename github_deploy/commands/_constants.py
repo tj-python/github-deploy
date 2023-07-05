37d65b7920448444cfa66ef59687999acda38d75
Tonye Jack <jtonye@ymail.com>
2023-07-05 12:11:56 -0600

    Update _constants.py

--- github_deploy/commands/_constants.py
+++ github_deploy/commands/_constants.py
@@ -1,2 +1,2 @@
-REPOS_URL = "https://api.github.com/users/{org}/repos"
+REPOS_URL = "https://api.github.com/users/{org}/repos?per_page=100"
 FILE_CONTENTS_URL = "https://api.github.com/repos/{repo}/contents/{path}"
